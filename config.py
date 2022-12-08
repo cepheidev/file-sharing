99642939feb6348d4cb96393159b1ae56bef8806
Cepheid <56135612+cepheidev@users.noreply.github.com>
2022-12-08 23:10:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from distutils.util import strtobool
 from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
 
-load_dotenv("config.env")
+
 
 # Bot token dari @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5632971423:AAGKoPHtE9zKvbuk8Nnc3xvWebVBn5pfATI")
